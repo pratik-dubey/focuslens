c42e6b27c0d20a2945668613d112d23e1d8f9bf6
Pratik Dubey <pratikdubey091@gmail.com>
2026-04-13 08:36:34 +0530

    fix: restore cors headers in next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,22 @@
 //   experimental: {
 //     serverActions: { allowedOrigins: ['localhost:3000'] }
 //   },
-//   images: { domains: ['lh3.googleusercontent.com'] }
+//   images: { 
+//     domains: ['lh3.googleusercontent.com'] 
+//   },
+
+//   async headers() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         headers: [
+//           { key: 'Access-Control-Allow-Origin', value: '*' },
+//           { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PATCH, OPTIONS' },
+//           { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+//         ],
+//       },
+//     ]
+//   },
 // }
 
 // export default nextConfig
@@ -11,13 +26,17 @@
 
 
 
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: { allowedOrigins: ['localhost:3000'] }
-  },
-  images: { 
-    domains: ['lh3.googleusercontent.com'] 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 
   async headers() {
